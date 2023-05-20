cd28dc6981f52a54173838fc37c596a94dbee09d
batuhansariikaya <66845211+batuhansariikaya@users.noreply.github.com>
2023-05-20 14:44:53 +0300

    Reflection Example

---
+++ ReflectionExample/ReflectionExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using ReflectionExample;
+using System.Reflection;
+
+Car car = new Car();
+
+car.GetType().GetProperties().ToList().ForEach(p =>
+{
+    if (p.Name == "No") 
+    {
+        p.SetValue(car, 1);
+    }
+    else if (p.Name == "Brand")
+    {
+        p.SetValue(car, "Peugeot");
+    }
+    else if (p.Name == "Model")
+    {
+        p.SetValue(car, "207");
+    }
+    else
+    {
+        p.SetValue(car, 2009);
+    }
+
+});
+Console.WriteLine($"Marka:{car.Brand} \nModel:{car.Model} \nYıl:{car.Year}");
